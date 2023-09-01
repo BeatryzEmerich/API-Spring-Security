c541a3dc8a9a9192c1f95ddb39843b428a9b8c19
BeatryzEmerich <biaemerich22@gmail.com>
2023-09-01 19:44:13 -0300

    Alteração de dependências, criação de classes controller e config
    Criação das classes de controle e configuração, também foi adicionado e alterado as dependências necessárias para o funcionamento do projeto

---
+++ src/main/java/dio/spring/security/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package dio.spring.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST,"/login").permitAll() //só vai ser permitido do tipo post
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS", "MANAGERS") //aqui tanto o Managers e os users podem acessar a rota do user
+                .anyRequest().authenticated().and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+                .roles("USERS") //case sensitive
+                .and()
+                .withUser("admin")
+                .password("{noop}master123")
+                .roles("MANAGERS");
+    }
+}
